3c8baa9557810eefbe63c0730b8f23e5ca96b555
Hung <hungnguyen150300@gmail.com>
2022-09-20 02:14:58 +0300

    Uploading sort app

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,6 +1,16 @@
 "use strict";
-console.log('123');
-const logSomething = () => {
-    console.log('Something');
-};
-logSomething();
+Object.defineProperty(exports, "__esModule", { value: true });
+const NumberCollection_1 = require("./NumberCollection");
+const numberCollection = new NumberCollection_1.NumberCollection([10, 3, -5, 0]);
+numberCollection.sort();
+console.log(numberCollection.data);
+// const characterCollection = new CharacterCollection('Xaayb');
+// characterCollection.sort();
+// console.log(characterCollection.data);
+// const linkedList = new LinkedList();
+// linkedList.add(500);
+// linkedList.add(-10);
+// linkedList.add(-3);
+// linkedList.add(4);
+// linkedList.sort();
+// linkedList.print();
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,19 @@
-console.log('123');
+import { NumberCollection } from './NumberCollection';
+import { CharacterCollection } from './CharacterCollection';
+import { LinkedList } from './LinkedList';
 
-const logSomething = () => {
-  console.log('Something');
-};
+const numberCollection = new NumberCollection([10, 3, -5, 0]);
+numberCollection.sort();
+console.log(numberCollection.data);
 
-logSomething();
+// const characterCollection = new CharacterCollection('Xaayb');
+// characterCollection.sort();
+// console.log(characterCollection.data);
+
+// const linkedList = new LinkedList();
+// linkedList.add(500);
+// linkedList.add(-10);
+// linkedList.add(-3);
+// linkedList.add(4);
+// linkedList.sort();
+// linkedList.print();
