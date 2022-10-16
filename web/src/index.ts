ad061b3e59108ea70467e4d9ca7c3295400f2b35
Hung <hungnguyen150300@gmail.com>
2022-10-16 12:29:31 +0300

    Uploading web files

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,8 +2,5 @@ import { User } from './models/User';
 
 const user = new User({ name: 'Other Name', age: 20 });
 
-user.events.on('change', () => {
-  console.log('change #1');
-});
-
-user.events.trigger('change');
+user.attributes.get('name');
+user.sync.save()
